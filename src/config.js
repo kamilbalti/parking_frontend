a25d7318f0e620648d5ddd2c2254dbf19fef5251
kamilbalti <mohammad.kamilbalti@gmail.com>
2024-02-19 22:29:35 +0500

    resolve auth error

--- src/config.js
+++ src/config.js
@@ -1,2 +1,1 @@
-export const url = 'http://localhost:4000'
-// export const url = 'https://parking-app-cy-0dbc5aa09492.herokuapp.com'
+export const url = 'https://parking-app-cy-0dbc5aa09492.herokuapp.com'
